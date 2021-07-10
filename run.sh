852c8035fcda6229ae5b235e8d2e7a84fa497fae
dadide <1712279967@qq.com>
2021-07-10 22:37:09 +0800

    first commit

--- run.sh
+++ run.sh
@@ -9,3 +9,13 @@ git remote -v
 git pull origin master
 git push origin master
 
+
+cd /home/wy/newIVHM/zlgminiCAN/
+rm IVHM.tar.gz
+tar zcvf /home/wy/newIVHM/zlgminiCAN/IVHM.tar.gz *
+cd /home/wy/df/0608
+cp /home/wy/newIVHM/zlgminiCAN/IVHM.tar.gz /home/wy/df/0714
+git add .
+git commit -m 'new'
+git pull origin main
+git push origin main
