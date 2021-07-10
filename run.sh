da63c5421fee2b56d85609c20210caf23e5dd884
dadide <1712279967@qq.com>
2021-07-10 22:25:33 +0800

    first commit

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 cd /home/wy/newIVHM/zlgminiCAN/
-tar zcvf /home/wy/df/0714/IVHM2.tar.gz *
+tar zcvf /home/wy/df/0714/IVHM.tar.gz *
 cd /home/wy/df/0714/
 git add .
 git commit -m 'first commit'
 git remote -v
-git remote add origin git@github.com:dadide/envir.git
-git remote -v
+#git remote add origin git@github.com:dadide/envir.git
+#git remote -v
 git pull origin master
 git push origin master
 
