880dcf01b9fb78974bbdd600282fbc2ca2e4355b
dadide <1712279967@qq.com>
2021-08-29 00:50:51 +0800

    first 0829

---
+++ run.sh
@@ -0,0 +1,21 @@
+cd /home/wy/newIVHM/zlgminiCAN/
+tar zcvf /home/wy/df/0714/IVHM.tar.gz *
+cd /home/wy/df/0714/
+git add .
+git commit -m 'first commit'
+git remote -v
+#git remote add origin git@github.com:dadide/envir.git
+#git remote -v
+git pull origin master
+git push origin master
+
+
+cd /home/wy/newIVHM/zlgminiCAN/
+rm IVHM.tar.gz
+tar zcvf /home/wy/newIVHM/zlgminiCAN/IVHM.tar.gz *
+cd /home/wy/df/0608
+cp /home/wy/newIVHM/zlgminiCAN/IVHM.tar.gz /home/wy/df/0714
+git add .
+git commit -m 'new'
+git pull origin master
+git push origin master
